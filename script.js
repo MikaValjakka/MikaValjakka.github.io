7324760f8d375a0a194c2b532c6cce6816eee5e9
MikaValjakka <mika.valjakka@hotmail.com>
2023-08-07 13:40:33 +0300

    Movement and blur added to mainpage. 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=> {
+     console.log(entry)
+     if(entry.isIntersecting) {
+        entry.target.classList.add('show');
+     }
+    });
+});
+
+const hiddenElementsl = document.querySelectorAll('.hiddenl');
+hiddenElementsl.forEach((el)=> observer.observe(el));
+
+const hiddenElementsr = document.querySelectorAll('.hiddenr');
+hiddenElementsr.forEach((el)=> observer.observe(el));
